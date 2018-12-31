466edc31642a80a9be757e151cfddc2390bb67f6
jamesal1 <jamesal1@ics.uci.edu>
2018-12-31 00:38:05 -0800

    fixed energy distance bug; was sorting on (1,n) array

--- energy_dist_test.py
+++ energy_dist_test.py
@@ -8,23 +8,41 @@ def energyDistance(activations, dist=None):
     # pdb.set_trace()
     x = activations[0:size]
     x_prime = activations[size:2 * size]
-    y = torch.rand(*x.shape) * 5 - 5 / 2
+    y = torch.rand(*x.shape) * 5 - 5/2
     y = Variable(y, requires_grad=False)
+    # print(y.sum())
+    # exit()
     dist = 0
-    for _ in range(1):
-        tmp = torch.rand((dim, 1))
+    for _ in range(5):
+        tmp = torch.rand((dim, 1))-.5
         tmp = tmp / torch.norm(tmp)
         projection = Variable(tmp, requires_grad=False)
+        x_proj, x_arg = torch.sort((x @ projection).flatten())
+        x_prime_proj, x_prime_arg = torch.sort((x_prime @ projection).flatten())
+        y_proj, y_arg = torch.sort((y @ projection).flatten())
+        # print(x_proj)
+        # print(y_proj)
+        # dist += torch.sum((y_proj - x_prime_proj) ** 2) + torch.sum((y_proj - x_proj) ** 2) - torch.sum((x_proj - x_prime_proj) ** 2)
+        dist += 2 * torch.sum((y_proj - x_proj) ** 2) - torch.sum((x_proj - x_prime_proj) ** 2)
 
-        x_proj, x_arg = torch.sort(x @ projection)
-        x_prime_proj, x_prime_arg = torch.sort(x_prime @ projection)
-        y_proj, y_arg = torch.sort(y @ projection)
-        dist += 2 * torch.sum((x_proj - y_proj) ** 2) - torch.sum((x_proj - x_prime_proj) ** 2)
+    return dist / 5
 
-    return dist / 1
 
-a = Variable(torch.rand(5,2),requires_grad=True)
-ed = energyDistance(a)
-print(ed)
-ed.backward()
-print(a.grad)
+
+
+
+a = Variable(torch.rand(40,2)-.5,requires_grad=True)
+optimizer = torch.optim.Adam([a], lr = .0001)
+print(a.t())
+for i in range(1000):
+    ed = energyDistance(a)
+    ed.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+print(a.t())
+from matplotlib import pyplot as plt
+
+at=a.t().detach().numpy()
+plt.scatter(at[0],at[1])
+plt.savefig("energy_test.png")
+plt.close()
